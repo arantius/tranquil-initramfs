6daadab9b84eebedcb27b7999b0fb977f6e0cc55
Jonathan Vasquez <jvasquez1011@gmail.com>
2012-06-24 16:16:37 -0400

    changed to correct LF, UTF-8 w/o BOM aka  ANSI with UTF-8

--- hooks/hook_lvm.sh
+++ hooks/hook_lvm.sh
@@ -1,4 +1,4 @@
-﻿# Copyright (C) 2012 Jonathan Vasquez
+# Copyright (C) 2012 Jonathan Vasquez
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
