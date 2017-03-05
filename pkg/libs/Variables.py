fd2a7f9f75ca3dd0f155d73e6e50e556d7809371
Jonathan Vasquez <jon@xyinn.org>
2017-03-04 19:12:21 -0500

    - Relicensed to 2-BSD - Fixed a bug where leaving the "Features [1]: " response empty wouldn't actually select ZFS.

--- pkg/libs/Variables.py
+++ pkg/libs/Variables.py
@@ -1,17 +1,25 @@
 # Copyright 2012-2017 Jonathan Vasquez <jon@xyinn.org>
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import subprocess
@@ -23,8 +31,8 @@ name = "Bliss Initramfs"
 author = "Jonathan Vasquez"
 email = "jon@xyinn.org"
 contact = author + " <" + email + ">"
-version = "7.0.1"
-license = "GPL v3.0"
+version = "7.0.2"
+license = "2-BSD"
 
 # Locations
 home = os.getcwd()
@@ -95,10 +103,10 @@ baselayout = [
 ]
 
 # Line numbers in the 'init' script where sed will substitute its values in
-useLuksLine = "21"
-useRaidLine = "22"
-useLvmLine = "23"
-useZfsLine = "24"
-useAddonLine = "25"
-initrdVersionLine = "19"
-addonModulesLine = "27"
+useLuksLine = "29"
+useRaidLine = "30"
+useLvmLine = "31"
+useZfsLine = "32"
+useAddonLine = "33"
+initrdVersionLine = "27"
+addonModulesLine = "35"
